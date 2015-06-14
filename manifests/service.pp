b73ac85d124be62a7fc8402fec8a7be7d592d2c8
Scott Konzem <konzems@gmail.com>
2015-06-14 00:49:17 -0400

    remove hasstatus => false to workaround PUP-4431

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,7 @@ class nullmailer::service {
   service { $nullmailer::service:
     ensure     => running,
     enable     => true,
-    hasstatus  => false,
+    #hasstatus  => false,
     hasrestart => true,
     require    => Class['nullmailer::config'],
   }
